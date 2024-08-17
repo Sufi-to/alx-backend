5dfbfdd92b97ce176a50c9cd04750c715147f096
Sufi-to <3thomas225@gmail.com>
2024-08-17 18:57:06 +0400

    index of returned data

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,17 +10,6 @@ def index_range(page, page_size):
     """
     if page == 0:
         return (1, page_size)
-    
+
     if page > 0:
         return ((page-1)*page_size, page*page_size)
-    
-    
-
-
-res = index_range(1, 7)
-print(type(res))
-print(res)
-
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
