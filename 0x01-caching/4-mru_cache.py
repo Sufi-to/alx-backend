c9c58da4537cd46121fcb8f074bc0749654ab1f3
Sufi-to <3thomas225@gmail.com>
2024-08-22 06:42:53 +0400

    implement mru caching system

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module that builds MRU Cache Replacement"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache implements a Most Recently Used (MRU) caching system """
+
+    def __init__(self):
+        """ Initialize the MRUCache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.order.pop() 
+                del self.cache_data[mru_key]
+                print(f"DISCARD: {mru_key}")
+
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
