810439290225f5198b21a3ff52303d2e4f4d2c4c
Sufi-to <3thomas225@gmail.com>
2024-08-27 22:32:58 +0400

    spin flask and setup babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'], strict_slashes=False)
 def hello_world():
     """Returns a simple page to render."""
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module for a spinning up a simple flask app with a babel object"""
+
+import requests
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Configuration class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """Returns a simple page to render."""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
