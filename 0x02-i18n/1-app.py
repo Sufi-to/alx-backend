879185487aee3723fc1d64587815450d2253e8b5
Sufi-to <3thomas225@gmail.com>
2024-08-30 09:02:33 +0400

    launch babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,9 +4,6 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
     """Configuration class for babel"""
@@ -14,8 +11,9 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
